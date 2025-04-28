dba23310d95b0ea8ae560c58325d3390966fa613
unknown <edadfeudal@gmail.com>
2025-04-27 23:02:41 -0300

    FINAL

--- mate/conversor_numeros.py
+++ mate/conversor_numeros.py
@@ -2,6 +2,7 @@ def decimal_a_binario(decimal):
     if decimal < 0:
         return "El número debe ser positivo."
     
+
     binario = ""
     if decimal == 0:
         return "0"
@@ -13,16 +14,31 @@ def decimal_a_binario(decimal):
     
     return binario
 
+
+
+# Al ingresar la opcion 2
+# BINARIO A DECIMAL 
 def binario_a_decimal(binario):
+    # Validamos que el numero binario sea digito 1 o 0 unicamente, recorriendolo con un for 
     if not all(c in '01' for c in binario):
         return "Número binario inválido."
     
+    # creamos el acumulador donde se sumara los digitos binarios
     decimal = 0
+
+    # Luego creamos un rango con el tamaño de la cantidad de digitos binarios y los vamos recorriendo 
+    # Cada digito de acuerdo a la posicion se elevará al cuadrado y luego los vamos sumando la variable decimal
     for i in range(len(binario)):
         exponente = len(binario) - 1 - i
         digito = int(binario[i])
         decimal += digito * 2 ** exponente
+    #Retornamos la variable decimal
     return decimal
+
+
+
+
+
 
 while True:
     print("\nSeleccione opción:")
